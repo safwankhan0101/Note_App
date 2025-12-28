fe536b000ae22c1849f48b16f0756aa90414361f
Safwan khan <safwankhan010101@gamil.com>
2025-12-28 20:48:36 +0500

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Apple Orchard on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesAppTests/NotesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppTests.swift
+//  NotesAppTests
+//
+//  Created by Apple Orchard on 28/12/2025.
+//
+
+import Testing
+@testable import NotesApp
+
+struct NotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
